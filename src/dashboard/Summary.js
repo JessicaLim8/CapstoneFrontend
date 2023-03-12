dbc1714e5b79c83d6a93b1024b44f224d9915c63
Jessica Lim <jessicalim813@gmail.com>
2023-03-12 15:29:28 -0400

    Add last attempt summary

--- src/dashboard/Summary.js
+++ src/dashboard/Summary.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 import Title from './Title';
 
 function preventDefault(event) {
@@ -10,27 +11,49 @@ function preventDefault(event) {
 export default function Summary(props) {
   return (
     <React.Fragment>
-      <Title>Summary</Title>
-      <Typography component="p" variant="h6">
-        {props.exercise}
-      </Typography>
-      <Typography color="text.secondary" sx={{ flex: 1 }}>
-        Max Force
-      </Typography>
-      <Typography component="p" variant="h5">
-        {props.max}
-      </Typography>
-      <Typography color="text.secondary" sx={{ flex: 1 }}>
-        Avg Force
-      </Typography>
-      <Typography component="p" variant="h5">
-        {props.avg}
-      </Typography>
-      <div>
-        <Link color="primary" href="#" onClick={preventDefault}>
-          View Details
-        </Link>
-      </div>
+      <Title>
+        Summary: Last Attempt
+      </Title>
+      <Grid container spacing={3}>
+        { props.left &&
+          <Grid item xs={props.left && props.right ? 6 : 12}>
+            <Typography component="p" variant="h6">
+              Left
+            </Typography>
+            <Typography color="text.secondary" sx={{ flex: 1 }}>
+              Max Force
+            </Typography>
+            <Typography component="p" variant="h5">
+              {props.left ? props.left.max + "N"  : "--"}
+            </Typography>
+            <Typography color="text.secondary" sx={{ flex: 1 }}>
+              Avg Force
+            </Typography>
+            <Typography component="p" variant="h5">
+              {props.left ? props.left.avg + "N" : "--"}
+            </Typography>
+          </Grid>
+        }
+        { props.right &&
+          <Grid item xs={props.left && props.right ? 6 : 12}>
+            <Typography component="p" variant="h6">
+              Right
+            </Typography>
+            <Typography color="text.secondary" sx={{ flex: 1 }}>
+              Max Force
+            </Typography>
+            <Typography component="p" variant="h5">
+              { props.right ? props.right.max + "N" : "--"}
+            </Typography>
+            <Typography color="text.secondary" sx={{ flex: 1 }}>
+              Avg Force
+            </Typography>
+            <Typography component="p" variant="h5">
+              {props.right ? props.right.avg + "N" : "--"}
+            </Typography>
+          </Grid>
+        }
+      </Grid>
     </React.Fragment>
   );
 }
